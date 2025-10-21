0577a16678067b2a0140f0f32454c3d76cb9cf13
RaynaldoHuang <huangraynaldo@gmail.com>
2025-10-21 18:50:50 +0700

    update padding

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -39,7 +39,7 @@ export default async function LocaleLayout({
       <NextIntlClientProvider locale={locale} messages={messages}>
         <NavigationBar />
         <main className='relative'>
-          <div className='pt-28' />
+          <div className='pt-24' />
           {children}
           <FloatingLanguageSelector />
         </main>
