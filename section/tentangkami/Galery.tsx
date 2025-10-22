e29bc15b4f975e6608dd8aea478396b3cfdcd10f
Juan Antonio <juananthonio23@gmail.com>
2025-10-22 15:23:38 +0700

    fix: adjust grid layout in GallerySection for improved responsiveness

--- section/tentangkami/Galery.tsx
+++ section/tentangkami/Galery.tsx
@@ -32,7 +32,7 @@ export default function GallerySection() {
             </p>
           </div>
 
-          <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-5'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-5'>
             <div>
               <Image
                 alt=''
