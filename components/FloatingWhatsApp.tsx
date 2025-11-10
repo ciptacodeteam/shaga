91ad8d732df5f1fb408724cba5e5c8d0c11dddbe
RaynaldoHuang <huangraynaldo@gmail.com>
2025-11-10 18:00:05 +0700

    update button wA

--- components/FloatingWhatsApp.tsx
+++ components/FloatingWhatsApp.tsx
@@ -1,31 +1,32 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { FloatingWhatsApp } from '@dxkit-org/react-floating-whatsapp'
+import React from "react";
+import { FloatingWhatsApp } from "@dxkit-org/react-floating-whatsapp";
 
 export default function FloatingWhatsApps() {
-    const hour = new Date().getHours()
-  let greeting = 'Halo! 👋'
+  const hour = new Date().getHours();
+  let greeting = "Halo! 👋";
 
-  if (hour < 12) greeting = 'Selamat pagi! ☀️'
-  else if (hour < 18) greeting = 'Selamat siang! 🌤️'
+  if (hour < 12) greeting = "Selamat pagi! ☀️";
+  else if (hour < 18) greeting = "Selamat siang! 🌤️";
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  else greeting = 'Selamat malam! 🌙'
+  else greeting = "Selamat malam! 🌙";
 
   return (
-   <>
-    <FloatingWhatsApp
-      phoneNumber='+628116383887'
-      accountName='PT. SHAGA'
-      avatar='/img/avatar.png'
-      statusMessage='Online'
-      chatMessage={`${greeting} Terima kasih sudah menghubungi PT. SHAGA. Ada yang bisa kami bantu hari ini?`}
-      darkMode={false}
-      allowEsc={true}
-      notification={true}
-      className='custom-whatsapp-widget'
-      notificationSound={true}
-    />
-   </>
-  )
+    <>
+      <FloatingWhatsApp
+        phoneNumber="+628116383887"
+        accountName="PT. SHAGA"
+        avatar="/img/avatar.png"
+        statusMessage="Online"
+        chatMessage={`${greeting} Ada yang bisa kami bantu hari ini?`}
+        className="custom-whatsapp-widget"
+        notification={true}
+        notificationSound={true}
+        darkMode={false}
+        allowEsc={true}
+        style={{ left: "20px", right: "unset", bottom: "20px" }} // 🔄 posisi kiri bawah
+      />
+    </>
+  );
 }
